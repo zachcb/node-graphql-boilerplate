eb44851d95bf4a4d0f632b301a21677fcc7de003
Zach <zach.c.barnes@gmail.com>
2021-02-28 13:20:41 -0800

    FEAT: Add create connection

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,53 @@
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
-import { logger } from './utils/logger';
-
-// Initialize a GraphQL schema
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "type-graphql";
+import cors from "cors";
+import { createInterface } from "./database";
+import { logger } from "./utils/logger";
+import { config } from "./config";
 
 // Root resolver
 const root = {
-  hello: () => 'Hello worl!',
+  hello: () => "Hello world!",
 };
 
-// Create an express server and a GraphQL endpoint
-const app = express();
+export const main = async (): Promise<void> => {
+  const PGInterface = createInterface();
+  PGInterface.connect();
 
-app.use('/graphql', graphqlHTTP({
-  schema, // Must be provided
-  rootValue: root,
-  graphiql: true, // Enable GraphiQL when server endpoint is accessed in browser
-}));
+  const schema = await buildSchema({
+    resolvers: [`${__dirname}/resolvers/**/*.resolver.?s`],
+    globalMiddlewares: [],
+  });
 
-app.listen(5000, () => logger({
-  message: 'Now browse to http://localhost:5000/graphql',
-  status: 'success',
-}));
+  // Create an express server and a GraphQL endpoint
+  const app = express();
+
+  // const corsOptions: cors.CorsOptions = {
+  //   origin: `${config.ORIGIN_URL}`,
+  //   credentials: false,
+  // };
+
+  app.use(cors());
+
+  app.get("/status", (req, res) => {
+    res.status(200).send("OK");
+  });
+
+  app.use("/graphql", graphqlHTTP({
+    schema, // Must be provided
+    rootValue: root,
+    graphiql: true, // Enable GraphiQL when server endpoint is accessed in browser
+  }));
+
+  app.listen(config.PORT, () => logger({
+    message: `
+      Node GraphQL Boilerplate
+      GraphQL: http://localhost:${config.PORT}/graphql
+    `,
+    status: "success",
+    box: true,
+  }));
+};
+
+main().catch((error) => console.error(error));
