381f46c6e03e1c688edf61bab2d5789ad1072ae3
Zach <zach.c.barnes@gmail.com>
2021-02-28 13:19:08 -0800

    FEAT: Add chalk, boxen, and cors

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,34 @@
-export const logger = (context: Record<string, unknown>): void => {
-  const { status, message } = context;
+import chalk from "chalk";
+import boxen from "boxen";
+
+const {
+  log, warn, error, table,
+} = console;
+type LoggerOptions = {
+  status: string;
+  message: string;
+  box?: boolean;
+}
+
+const boxOptions: boxen.Options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "double",
+};
+
+export const logger = (context: LoggerOptions): void => {
+  const { status, message, box = false } = context;
+  const preparedMessage = box ? boxen(message, boxOptions) : message;
 
   switch (status) {
-    case 'success':
-      console.log('LOG', message);
+    case "success":
+      log(chalk.blue(preparedMessage));
       break;
-    case 'error':
-      console.error('ERROR', message);
+    case "warn":
+      warn(chalk.yellow(preparedMessage));
+      break;
+    case "error":
+      error(chalk.red(preparedMessage));
       break;
     default:
   }
