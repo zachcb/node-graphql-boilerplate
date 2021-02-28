02ed4694dd672d441629088b009fa3b1f497bd6d
Zach <zach.c.barnes@gmail.com>
2021-02-28 13:18:26 -0800

    FIX: Entities not being assigned

--- src/database/index.ts
+++ src/database/index.ts
@@ -9,39 +9,72 @@ import {
   Connection,
   createConnection,
   useContainer,
-} from 'typeorm';
 
-import { config } from '../config';
+  ConnectionOptions,
+  Any,
+  getConnection,
+} from "typeorm";
+import { User } from "./models/user";
+import { Profile } from "./models/profile";
+import { Post } from "./models/post";
+import { config } from "../config";
 
-const interfaceOptions = {
-  postgres: {
-    type: 'postgres',
-    name: config.PG_NAME,
-    host: config.PG_HOST,
-    database: config.PG_DATABASE,
-    port: config.PG_PORT,
-    username: config.PG_USER,
-    password: config.PG_PASSWORD,
-    ssl: config.PRODUCTION,
-    synchronize: true,
-    logging: !config.PRODUCTION,
-    entities: ['database/models/*.{js,ts}'],
-    dropSchema: true,
-    cache: true,
-  },
-};
+const connectionOptions: Array<ConnectionOptions> = [{
+  type: "postgres",
+  name: config.PG_NAME,
+  host: config.PG_HOST,
+  database: config.PG_DATABASE,
+  port: config.PG_PORT,
+  username: config.PG_USER,
+  password: config.PG_PASSWORD,
+  ssl: config.PRODUCTION,
+  synchronize: true,
+  logging: !config.PRODUCTION,
+  // entities: [`${__dirname}/database/models/**/*.{js,ts}`],
+  entities: [User, Profile, Post],
+  dropSchema: true,
+  cache: true,
+}];
 
-export default function createInterface() {
-  const context = {
+type PostgresInterface = {
+  connection: Record<string, unknown>;
+  connect: () => void;
+  getConnection: (name: string) => Promise<Connection>;
+  disconnect: () => void;
+  executeSQL: (
+    sql: string,
+    params: Record<string, unknown>,
+  ) => Promise<Record<string, unknown>>;
+  reset: () => void;
+  runMigrations: () => void;
+  dropDatabase: () => void;
+}
+
+export function createInterface(): PostgresInterface {
+  const context: PostgresInterface = {
+    connection: {},
+
     connect: async () => {
-      this.connection = await createConnection(interfaceOptions);
+      const [postgresConnectionOptions] = connectionOptions;
+      this.connection = await createConnection({
+        ...postgresConnectionOptions,
+      });
     },
+
+    getConnection: async (name): Promise<Connection> => {
+      return getConnection(name);
+    },
+
     disconnect: async () => {
       if (this.connection.isConnected) {
         await this.connection.close();
       }
     },
-    executeSQL: async (sql, params) => {
+
+    executeSQL: async (
+      sql: string,
+      params: Record<string, unknown>,
+    ): Promise<Record<string, unknown>> => {
       return this.connection.createQueryRunner().query(sql, params);
     },
 
